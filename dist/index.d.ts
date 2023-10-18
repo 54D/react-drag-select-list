8d420dfa16f8c4532ac691f2e33c07f9a65b4dda
54D <fivefourdee@gmail.com>
2023-10-18 09:51:46 +0800

    fix: unignore dist folder

---
+++ dist/index.d.ts
@@ -0,0 +1,22 @@
+import React, { HTMLAttributes, CSSProperties } from 'react';
+
+type DragSelectListProps<T> = HTMLAttributes<HTMLDivElement> & {
+    selectionMode?: 'default' | 'append' | 'remove';
+    containerStyle?: CSSProperties;
+    selectorStyle?: CSSProperties;
+    onSelectionChange?: (selectedIndices: number[]) => void;
+    data?: T[];
+    dataKeyExtractor?: (data: T) => string;
+    renderItem?: (data: T, index?: number) => JSX.Element;
+    children: JSX.Element;
+};
+declare const DragSelectList: <DataType>({ selectionMode, containerStyle, selectorStyle, onSelectionChange, data, dataKeyExtractor, renderItem, children, ...props }: DragSelectListProps<DataType>) => React.JSX.Element;
+
+type DragSelectListItemProps = HTMLAttributes<HTMLDivElement> & {
+    dataKey: string;
+    data: any;
+    innerRef?: (ref: HTMLDivElement) => void;
+};
+declare const DragSelectListItem: ({ dataKey, data, innerRef, onSelect, children, ...props }: DragSelectListItemProps) => React.JSX.Element;
+
+export { DragSelectList, DragSelectListItem };
