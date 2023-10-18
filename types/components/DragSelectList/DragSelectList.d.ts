218e60b7a5f60a11ca37c4e988fe6be98bd2ae12
54D <fivefourdee@gmail.com>
2023-10-18 09:38:11 +0800

    build:

--- types/components/DragSelectList/DragSelectList.d.ts
+++ types/components/DragSelectList/DragSelectList.d.ts
@@ -1,10 +1,13 @@
 import React, { CSSProperties, HTMLAttributes } from "react";
-export type DragSelectListProps = HTMLAttributes<HTMLDivElement> & {
+export type DragSelectListProps<T> = HTMLAttributes<HTMLDivElement> & {
     selectionMode?: 'default' | 'append' | 'remove';
     containerStyle?: CSSProperties;
     selectorStyle?: CSSProperties;
     onSelectionChange?: (selectedIndices: number[]) => void;
+    data?: T[];
+    dataKeyExtractor?: (data: T) => string;
+    renderItem?: (data: T, index?: number) => JSX.Element;
     children: JSX.Element;
 };
-declare const DragSelectList: ({ selectionMode, containerStyle, selectorStyle, onSelectionChange, children, ...props }: DragSelectListProps) => React.JSX.Element;
+declare const DragSelectList: <DataType>({ selectionMode, containerStyle, selectorStyle, onSelectionChange, data, dataKeyExtractor, renderItem, children, ...props }: DragSelectListProps<DataType>) => React.JSX.Element;
 export default DragSelectList;
